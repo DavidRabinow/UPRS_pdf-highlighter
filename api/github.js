0e727a71610d25bbe03320eda277c3dd77fd462b
David Rabinow <157258504+DavidRabinow@users.noreply.github.com>
2024-10-15 01:08:18 -0400

    github attempted pic update

--- api/github.js
+++ api/github.js
@@ -12,10 +12,10 @@ export default async (req, res) => {
 			if ((req.query.since && !req.query.until) || (!req.query.since && req.query.until) || !moment(req.query.since).isValid() || !moment(req.query.until).isValid()) return res.status(400).json({ success: false, code: 400, error: "invalid date range", args: req.query });
 			let since = req.query.since ? moment(req.query.since).toISOString() : moment().subtract(7, "d").toISOString(),
 				until = req.query.until ? moment(req.query.until).add(1, "d").toISOString() : moment(),
-				commits = await (await fetch(`https://api.github.com/repos/imaperson1060/imaperson.dev/commits?per_page=100&since=${since}&until=${until}`, { headers: { Authorization: `Basic ${btoa(`imaperson1060:${process.env.GITHUB_TOKEN}`)}` } })).json();
+				commits = await (await fetch(`https://api.github.com/repos/davidrabinow/imaperson.dev/commits?per_page=100&since=${since}&until=${until}`, { headers: { Authorization: `Basic ${btoa(`davidrabinow:${process.env.GITHUB_TOKEN}`)}` } })).json();
 			for await (const commit of commits) {
 				if (!(await query("SELECT * FROM `commits` WHERE id=?", [ commit.sha ]))[0]) {
-					commit.stats = (await (await fetch(`https://api.github.com/repos/imaperson1060/imaperson.dev/commits/${commit.sha}`, { headers: { Authorization: `Basic ${btoa(`imaperson1060:${process.env.GITHUB_TOKEN}`)}` } })).json()).stats;
+					commit.stats = (await (await fetch(`https://api.github.com/repos/davidrabinow/imaperson.dev/commits/${commit.sha}`, { headers: { Authorization: `Basic ${btoa(`imaperson1060:${process.env.GITHUB_TOKEN}`)}` } })).json()).stats;
 					await query("INSERT INTO `commits`(`id`, `name`, `author`, `stats`, `date`) VALUES (?,?,?,?,?)", [ commit.sha, encodeURIComponent(commit.commit.message), encodeURIComponent(commit.commit.author.name), JSON.stringify(commit.stats), commit.commit.author.date ]);
 				} else commit.stats = JSON.parse((await query("SELECT * FROM `commits` WHERE id=?", [ commit.sha ]))[0].stats);
 			}
