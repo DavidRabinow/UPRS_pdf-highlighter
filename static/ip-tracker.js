9c56b9844806d08d255d362496ea379f2b6fe95a
David Rabinow <157258504+DavidRabinow@users.noreply.github.com>
2025-03-12 17:29:41 -0400

    Revert "Update IP tracker implementation and configuration files"
    This reverts commit bdb467b1a87d21f169f8a773a7d322929e6a40df.

--- static/ip-tracker.js
+++
@@ -1,7 +0,0 @@
-// Add this to your website
-fetch('/.netlify/functions/getIp')
-    .then(response => response.json())
-    .then(data => {
-        console.log('Visitor IP:', data.ip);
-        // You can send this to your own server or database if needed
-    });
